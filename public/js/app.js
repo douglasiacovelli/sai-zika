3f32f49cbc706669a6096ce7eff22e3c22a675c8
Douglas Iacovelli <douglas.iacovelli@gmail.com>
2015-12-18 10:46:33 -0200

    removendo cache sass. Inicio da localizacao do usuario

--- public/js/app.js
+++ public/js/app.js
@@ -9,18 +9,83 @@ Parse.initialize("xfMgYWfJOTbqqSkeiMRlVZxW1xDE5X2yLNMeWnJj", "P9kIWs1glJqeyqemuZ
 
 var Checklist = Parse.Object.extend("Checklist");
 var checklist = new Checklist();
+var autocomplete;
 
 $("form").submit(function(e){
 	e.preventDefault();
-	var responses = {};
-	$("input[type=checkbox]").each(function(i,obj){
-		responses[obj.id] = obj.checked;
-	});
-	checklist.save(responses).then(function(object){
-		alert("Muito Obrigado! Suas respostas foram salvas");
-	});
+  //Verifica se endereço está vazio
+  if(!$("#endereco").val()){
+    alert("Por gentileza, preencha o campo endereço");
+  }else{
+    var place = autocomplete.getPlace();
+    var possibleStreetNumber = place['address_components'][0];
+    if(possibleStreetNumber['types'][0] == "street_number" || enderecoInput.data("lat")){
+      var respostas = {};
+      // Obtém todas as respostas e insere no dicionário respostas
+      $("input[type=checkbox]").each(function(i,obj){
+         respostas[obj.id] = obj.checked;
+      });
+      
+      //Salvar as respostas no BD do Parse como Objeto "Checklist"
+      // checklist.save(respostas).then(function(object){
+      //     alert("Muito Obrigado! Suas respostas foram salvas");
+      // });
+    }else{
+      alert("O endereço parece incompleto. Lembre-se de inserir o número");
+    }
+
+    
+  }
 });
 
+var enderecoInput = $("#endereco");
+var enderecoBuscado = false;
+//Define um listener para o click no input endereço para que seja obtida
+//a localização atual da pessoa
+enderecoInput.click(function(){
+  if(!enderecoBuscado){
+    enderecoBuscado = true;
+    getLocation();
+  }
+});
+
+//Função para obter a localização da pessoa pelo HTML5 e então converter
+//as coordenadas em um endereço
+function getLocation(){
+    if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var coordenadas = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      enderecoInput.data("lat", coordenadas.lat);
+      enderecoInput.data("lng", coordenadas.lng);
+
+      var geocoder = new google.maps.Geocoder;
+      geocoder.geocode({'location': coordenadas}, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+          if (results[0]) {
+            enderecoInput.val(results[0].formatted_address);
+          }
+        }
+      });
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  }
+}
+
+function initAutocomplete() {
+  // Create the autocomplete object, restricting the search to geographical
+  // location types.
+  autocomplete = new google.maps.places.Autocomplete(
+      (document.getElementById('endereco')),
+      {types: ['geocode']});
+
+  // When the user selects an address from the dropdown, populate the address
+  // fields in the form.
+  //autocomplete.addListener('place_changed', atualizarCoordenadas);
+}
 
 function initMap() {
   // Specify features and elements to define styles.
